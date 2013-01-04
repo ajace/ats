62e52b166fd2218cb2a813d2a319852ac858f2b0
Ace Atienza <ace@ace.com>
2013-01-03 21:44:23 -0500

    change db from sqlite to postgresql

--- Gemfile
+++ Gemfile
@@ -1,11 +1,12 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.9'
+gem 'pg', '0.12.2'
 
 group :development, :test do
-  gem 'sqlite3'
-  gem 'rspec-rails'
-  gem 'capybara'
+  # gem 'sqlite3', '1.3.5'
+  gem 'rspec-rails', '2.11.0'
+  gem 'capybara', '1.1.2'
 end
 
 
@@ -17,8 +18,6 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
-gem 'jquery-rails'
+gem 'jquery-rails', '2.0.2'
 
-group :production do
-  gem 'pg'
-end
+
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,22 +1,25 @@
 require 'spec_helper'
 # include Capybara::DSL
 
-describe "StaticPages" do
+describe "Static pages" do
 
-  describe "GET /static_pages/about" do
-    it "About page should have content 'Applicant Tracking System'" do
+  describe "About page" do
+    it "should have content 'Applicant Tracking System'" do
       visit '/static_pages/about'
-      page.should have_content('Applicant Tracking System')
+      page.should have_content('About')
     end
+
+    it "should have the right title" do
+	  	visit '/static_pages/about'
+	  	page.should have_selector('title', :text => "Applicant Tracking System")
+      # the above generates a bug in Capybara 2.0. I also read the bug but did not think to go back (hence Mr. Hartl's request in the very beginning of his tutorial to set the gems to the right version.)
+      # http://stackoverflow.com/questions/14042731/rspec-not-matching-title
+      # response.should contain("Applicant Tracking System"); still incorrect
+      # response.should have_selector('title', :text => "Applicant Tracking System")
+      # response.should have_title("Applicant Tracking System")
+  	end
   end
 
+
+
 end
-
-# Still causes undefinte method error on visit without 'include Capybara::DSL'
-# feature "about page" do
-# 	scenario "visit about page" do
-# 		visit '/static_pages/about'
-#     	page.should have_content('Applicant Tracking System') 
-# 	end
-# end
-
