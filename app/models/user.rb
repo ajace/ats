c2eacd0b0be5a81b9fd291b5304d1617f9f225cf
Ace Atienza <ace@ace.com>
2013-01-13 15:37:44 -0500

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   validates_uniqueness_of :email
-  # validates_presence_of :password, :on => create
-  validates_format_of :email, with: /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
+  validates_format_of :email, with: /^.+@.+$/
+  validates_presence_of :password  # , :on => create; generates issues with Rspec
+  
 end
