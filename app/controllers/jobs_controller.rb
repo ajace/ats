33fd98c4ff48e908bbcbf4c5634f66ad4a9f6c2f
Ace Atienza <ace@ace.com>
2013-02-16 12:47:26 -0500

    add active tab and a styling

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,35 +1,17 @@
 class JobsController < ApplicationController
   # GET /jobs
-  # GET /jobs.json
   def index
     @jobs = Job.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @jobs }
-    end
   end
 
   # GET /jobs/1
-  # GET /jobs/1.json
   def show
     @job = Job.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @job }
-    end
   end
 
   # GET /jobs/new
-  # GET /jobs/new.json
   def new
     @job = Job.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @job }
-    end
   end
 
   # GET /jobs/1/edit
@@ -38,46 +20,33 @@ class JobsController < ApplicationController
   end
 
   # POST /jobs
-  # POST /jobs.json
   def create
     @job = Job.new(params[:job])
 
-    respond_to do |format|
       if @job.save
-        format.html { redirect_to @job, notice: 'Job was successfully created.' }
-        format.json { render json: @job, status: :created, location: @job }
+        redirect_to @job, notice: 'Job was successfully created.'
       else
-        format.html { render action: "new" }
-        format.json { render json: @job.errors, status: :unprocessable_entity }
+        render action: "new"
       end
-    end
   end
 
   # PUT /jobs/1
-  # PUT /jobs/1.json
   def update
     @job = Job.find(params[:id])
 
-    respond_to do |format|
+    
       if @job.update_attributes(params[:job])
-        format.html { redirect_to @job, notice: 'Job was successfully updated.' }
-        format.json { head :no_content }
+        redirect_to @job, notice: 'Job was successfully updated.'
       else
-        format.html { render action: "edit" }
-        format.json { render json: @job.errors, status: :unprocessable_entity }
+        render action: "edit"
       end
-    end
   end
 
   # DELETE /jobs/1
-  # DELETE /jobs/1.json
   def destroy
     @job = Job.find(params[:id])
     @job.destroy
 
-    respond_to do |format|
-      format.html { redirect_to jobs_url }
-      format.json { head :no_content }
-    end
+    redirect_to jobs_url
   end
 end
